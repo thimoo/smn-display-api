b4d614935a6a3a6bc4981fc4d3347ea8cef6fc70
Joel Gugger <joel@thimoo.ch>
2016-10-25 17:53:06 +0200

    removing stuff

--- routes/web.php
+++ routes/web.php
@@ -18,35 +18,35 @@ use App\Profile;
 // All routes are in api group
 
 Route::get('/', function(){
-    $p = Profile::find('jun');
-    $d = Data::first();
+    // $p = Profile::find('jun');
+    // $d = Data::first();
 
-    $collection = Value::getCollectionFor($p, $d);
-    $last = $collection->last();
+    // $collection = Value::getCollectionFor($p, $d);
+    // $last = $collection->last();
 
-    // $v = $p->values($d)->where('date', '2016-10-25 11:20:00')->first();
-    // $v->tag = 'substituted';
-    // $v->save();
+    // // $v = $p->values($d)->where('date', '2016-10-25 11:20:00')->first();
+    // // $v->tag = 'substituted';
+    // // $v->save();
 
-    $newValue = new Value([
-        'data_code' => 'temp',
-        'profile_stn_code' => 'cha',
-        'date' => '2016-10-25 11:20:00',
-        'value' => 11.5,
-        'tag' => 'original',
-    ]);
+    // $newValue = new Value([
+    //     'data_code' => 'temp',
+    //     'profile_stn_code' => 'cha',
+    //     'date' => '2016-10-25 11:20:00',
+    //     'value' => 11.5,
+    //     'tag' => 'original',
+    // ]);
 
-    // 8.34
-
-
-    // 11.5
+    // // 8.34
 
 
-    return Value::getCollectionFor($p, $d);
-    // $collection = Value::getSubstitutedLastValues($p, $d);
-
-    // Value::smoothSubstitutedValues($newValue, $collection);
+    // // 11.5
 
 
-    // return $collection;
+    // return Value::getCollectionFor($p, $d);
+    // // $collection = Value::getSubstitutedLastValues($p, $d);
+
+    // // Value::smoothSubstitutedValues($newValue, $collection);
+
+
+    // // return $collection;
 });
