2aed3835d540d927a1df9325077c0119c2b482df
Joel Gugger <joel@thimoo.ch>
2017-01-13 12:10:23 +0100

    add fake graph command

---
+++ app/Console/Commands/FakeGraph.php
@@ -0,0 +1,151 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Data;
+use App\Value;
+use App\Profile;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+
+class FakeGraph extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'fake:graph 
+                            {profile : The stn_code of the profile}
+                            {data : The code of the data}
+                            {noData? : The number of no-data}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Make a fake graphic for the given profile and data';
+
+    /**
+     * Define the default number of continious no-data value in the graph
+     * 
+     * @var integer
+     */
+    protected $noDataNumber = 6;
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $stnCode = $this->argument('profile');
+        $code = $this->argument('data');
+        
+        $this->noDataNumber = ($this->argument('noData') === null) ? $this->noDataNumber : (int) $this->argument('noData');
+
+        try
+        {
+            $collection = collect();
+            $profile = Profile::findOrFail($stnCode);
+            $data = Data::findOrFail($code);
+
+            Value::where('profile_stn_code', $stnCode)
+                    ->where('data_code', $code)
+                    ->delete();
+            
+            $date = new Carbon();
+            $date->subSeconds($date->second);
+            $date->subMinutes($date->minute % 10);
+            $date->subHours(24);
+
+            for ($i=0; $i < 144; $i++) { 
+
+                $date = new Carbon($date);
+
+                $last = $collection->get($i-1);
+                list($value, $tag) = $this->getRandomValue($last);
+
+                $v = new Value([
+                    'data_code' => $code,
+                    'profile_stn_code' => $stnCode,
+                    'date' => $date,
+                    'value' => $value,
+                    'tag' => $tag,
+                ]);
+                $collection[] = $v;
+
+                $date->addMinutes(10);
+
+            }
+
+            foreach ($collection as $value) {
+                $value->save();
+            }
+
+        }
+        catch (Exception $e)
+        {
+            $this->error('Something went wrong!');
+        }
+    }
+
+    /**
+     * Use to generate random and uniform values
+     * 
+     * @return array the value and the tag
+     */
+    protected function getRandomValue(Value $prev = null)
+    {
+        $tab = [];
+
+        if ($prev != null)
+        {
+            $diff = rand(-40, 40) / 10;
+            $nextValue = $prev->value + $diff;
+
+            switch ($prev->tag) {
+                case Value::NODATA:
+                    if ($this->noDataNumber > 0) {
+                        $tab = [0, Value::NODATA];
+                        $this->noDataNumber--;
+                    }
+                    else 
+                    {
+                        $tab = [$nextValue, Value::ORIGINAL];
+                    }
+                    break;
+
+                default:
+                    if ($this->noDataNumber > 0 && rand(0, 99) >= 95) 
+                    {
+                        $tab = [0, Value::NODATA];
+                        $this->noDataNumber--;
+                    }
+                    else 
+                    {
+                        $tab = [$nextValue, Value::ORIGINAL];
+                    }
+                    break;
+            }
+        }
+        else 
+        {
+            $tab = [rand(-20, 30), Value::ORIGINAL];
+        }
+
+        return $tab;
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,6 +15,7 @@ class Kernel extends ConsoleKernel
     protected $commands = [
         Commands\RefreshDatabase::class,
         Commands\RefreshProfiles::class,
+        Commands\FakeGraph::class,
     ];
 
     /**
