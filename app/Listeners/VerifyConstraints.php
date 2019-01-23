a6b66d2c4fc3a64b07266800c6cbc56092f7a279
Eric Monnier <eric@thimoo.ch>
2019-01-23 10:52:10 +0100

    delete messages

--- app/Listeners/VerifyConstraints.php
+++ app/Listeners/VerifyConstraints.php
@@ -41,18 +41,23 @@ class VerifyConstraints
         // Get the profile and the data attach to the
         // new value given
         $this->profile = Profile::find($value->profile_stn_code);
-        $this->data = Data::find($value->data_code);
+        $data_codes = array_unique(array_column($event->values, 'data_code'));
 
-        // If the data is null, then the data is not
-        // present in database and the value must be
-        // ignored
-        if ($this->data)
-        {
-          // Get the collection for the data and the profile
-          // and trigger the CheckConstraints event
-          $collection = Value::getCollectionFor($this->profile, $this->data);
+        foreach ($data_codes as $data_code) {
 
-          event(new CheckConstraints($this->profile, $this->data, $collection));
+          $this->data = Data::find($data_code);
+
+          // If the data is null, then the data is not
+          // present in database and the value must be
+          // ignored
+          if ($this->data)
+          {
+            // Get the collection for the data and the profile
+            // and trigger the CheckConstraints event
+            $collection = Value::getCollectionFor($this->profile, $this->data);
+
+            event(new CheckConstraints($this->profile, $this->data, $collection));
+          }
         }
     }
 }
